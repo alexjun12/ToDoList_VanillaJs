81401217bae1728a94a8228e116808abaee0d6b5
whwnsduddl <alexjun12@naver.com>
2021-12-27 15:54:57 +0900

    27commit

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-const player = {
-    name : 'jun',
-    sayHello : function(a){
-        console.log(a + "hello");
-    } ,
-};
-
-player.sayHello("hello!! ")
+const age = parseInt(prompt('HowOld?', 'Insert Age'));
+function ageMesure(age){
+    if(isNaN(age)) {
+        alert('Insert Number');
+    }
+    else if(age >= 20){
+        alert('you can drink')
+    }
+    else{
+         alert('you are too young')
+    }
+}
+ageMesure(age);
