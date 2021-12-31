5f2525bfae53ef083e11d108df7540e0070e887c
whwnsduddl <alexjun12@naver.com>
2021-12-31 21:32:34 +0900

    31commit

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-const age = parseInt(prompt('HowOld?', 'Insert Age'));
-function ageMesure(age){
-    if(isNaN(age)) {
-        alert('Insert Number');
-    }
-    else if(age >= 20){
-        alert('you can drink')
-    }
-    else{
-         alert('you are too young')
-    }
+const title = document.querySelector("div.hello:first-child h1");
+
+function isTitleClicked(){
+    title.style.color = 'red';
 }
-ageMesure(age);
+function isMoouseHere(){
+    title.style.color = "blue";
+}
+function isMouseNotHere(){
+    title.style.color = "black";
+}
+function ifResized(){
+    document.body.style.backgroundColor = "tomato";
+}
+function ifCopied(){
+    alert("COPIER!!");
+}
+
+title.addEventListener("click",isTitleClicked);
+title.addEventListener("mouseenter",isMoouseHere);
+title.addEventListener("mouseleave",isMouseNotHere);
+window.addEventListener("resize",ifResized);
+window.addEventListener("copy",ifCopied);
